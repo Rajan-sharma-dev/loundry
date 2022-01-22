3f9b18e198a73116de3c53b052aa5b1cf2f876c4
G Bhanu Prasad <gubbalausha11@gmail.com>
2022-01-22 23:18:52 +0530

    Bhanu-1

--- clieant/src/components/login.js
+++ clieant/src/components/login.js
@@ -1,9 +1,25 @@
+import React from "react";
 
-
-const Login=()=>{
-    return(<>
-        <h1>Rajana</h1>
-        </>)
+const Login = () => {
+    return (<>
+        <nav>
+            <label class="logo"> LAUNDRY </label>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#Pricing">Pricing</a></li>
+                <li><a href="#Career">Career</a></li>
+                <li><a href="#Sign In">Sign In</a></li>
+            </ul>
+        </nav>
+        {/* <div>
+            <h1>Laundry Service</h1>
+        </div>
+        <form>
+            <div className="form-inner">
+                <h2>SIGN IN</h2>
+            </div>
+        </form> */}
+    </>)
 }
 
 export default Login
