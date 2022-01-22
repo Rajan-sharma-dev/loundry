8bfcbc3d3790de879e84d8e043d33d87cd53b1e8
Rajan <rajan121.vishal121@gmail.com>
2022-01-22 14:54:05 +0530

    rajan-1

--- clieant/src/App.js
+++ clieant/src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Login from './components/login';
+import Registration from './components/register';
+import { Route,Routes } from 'react-router-dom';
 function App() {
   return (
-   <><h1>Hello world    uuuuuhgygyg</h1></>
+   <>
+   <Routes>
+   <Route  element={<Login />} path="/login"/>
+   <Route element={<Registration />} path="/registeration"/>
+   </Routes>
+   </>
   );
 }
 
---
+++ clieant/src/components/register.js
@@ -0,0 +1,10 @@
+
+
+const  Registration = ()=>{
+    return(<>
+        <h1>Registration</h1>
+        </>)
+
+}
+
+export default Registration
