8b0aeebe0e513ff5585ac7d669cf942a2f8fd12b
G Bhanu Prasad <gubbalausha11@gmail.com>
2022-01-23 20:24:56 +0530

    Bhanu-3

--- clieant/src/components/login.js
+++ clieant/src/components/login.js
@@ -1,16 +1,90 @@
 import React from "react";
+import fb from '../components/images/facebook.svg';
+import insta from '../components/images/instagram.svg';
+import lkdn from '../components/images/linkedin.svg';
+import lock from '../components/images/padlock';
 
 const Login = () => {
     return (<>
-        <nav>
-            <label class="logo"> LAUNDRY </label>
-            <ul>
-                <li><a class="active" href="#home">Home</a></li>
-                <li><a href="#Pricing">Pricing</a></li>
-                <li><a href="#Career">Career</a></li>
-                <li><a href="#Sign In">Sign In</a></li>
-            </ul>
-        </nav>
+        <div className='main-container'>
+            <div className='top-container'>
+                <div className='left'>
+                    <h1>LAUNDRY</h1>
+                </div>
+                <div className='right'>
+                    <div className='one'>
+                        Home
+                    </div>
+                    <div className='two'>
+                        Pricing
+                    </div>
+                    <div className='three'>
+                        Career
+                    </div>
+                    <div className='four'>
+                        <button className='btn'><p>Sign In</p></button>
+                    </div>
+                </div>
+            </div>
+            <div className='mid-container'>
+                <div className='left-part'>
+                    <h1 className='l1'>Laundry Service</h1>
+                    <br /><br />
+                    <p className='l2'>Doorstep Wash & Dryclean Service</p>
+                    <br />
+                    <br />
+                    <p1 className='l3'>Don't Have An Account?</p1>
+                    <br />
+                    <br />
+                    <button className='l4' >Register</button>
+                </div>
+                <div className='right-part'>
+                    <div className='r1'>
+                        <h4>SIGN IN</h4>
+                    </div>
+                    <form >
+                        <label className="label1">Mobile/Email</label>
+                        <br />
+                        <input name="email" className="email" type="text" />
+                        <br />
+                        <label className="label2">password</label>
+                        <br />
+                        <input name="password" className="password" type="password" />
+                        <img src={lock} alt='lock' />
+                        <h6>forgot password?</h6>
+                        <button className="signinbtn">sign in</button>
+                    </form>
+                </div>
+            </div>
+            <div className="footercontainer">
+                <div className='footerpara'>
+                    <hr style={{ color: '#5861AE' }} />
+                    <p className="para1">
+                        Now Refer & Earn &#8377;500 for every referral*
+                    </p>
+                    <p className="para2">*Terms and conditions will be applied</p>
+                </div>
+                <div className="footer">
+                    <p className="aboutus">ABOUT US</p>
+                    <p className="doorstep">Doorstep Wash & Dryclean Service</p>
+                    <p className="home">Home</p>
+                    <p className="signin">Sign In</p>
+                    <p className="register">Register</p>
+                    <p className="pricing">Pricing</p>
+                    <p className="career">Carrer</p>
+                    <p className="blogs">Blogs</p>
+                    <p className="create">Create</p>
+                    <p className="contact">Contact</p>
+                    <p className="socialmedia">SOCIAL MEDIA</p>
+                    <img src={fb} alt="fb" className='fb' />
+                    <img src={insta} alt="insta" className='insta' />
+                    <img src={lkdn} alt="lkdn" className='lkdn' />
+                </div>
+                <div className="footer1">
+                    <p class="lastpara">2021&#169;Laundry</p>
+                </div>
+            </div>
+        </div>
 
 
 
@@ -18,4 +92,4 @@ const Login = () => {
     </>)
 }
 
-export default Login
+export default Login;
